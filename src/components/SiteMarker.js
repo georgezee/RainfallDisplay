d09c000d34bb5c627b6a90a15cb23aea51f7a1e9
George Ziady <george@springfisher.com>
2020-09-04 22:28:17 +0200

    Replace map marker with circle.

--- src/components/SiteMarker.js
+++ src/components/SiteMarker.js
@@ -1,30 +1,37 @@
-import React, { Fragment } from 'react';
-import {Marker, Circle} from 'react-leaflet';
-//import {VenueLocationIcon} from './VenueLocationIcon';
+import React, { Component, Fragment } from 'react';
+import {Circle} from 'react-leaflet';
 import MarkerPopup from './MarkerPopup';
 
-const SiteMarker = (props) => {
-  const { sites } = props;
+export default class SiteMarker extends Component {
 
-  if (sites) {
-    const markers = sites.map((site, index) => (
-      <Marker
-        key={index}
-        position={site.geometry}
-        // icon={VenueLocationIcon}
-      >
+  calculateCircleSize = (input) => {
+    let dataMax = 1000;
+    let sizeMax = 300;
 
-        <MarkerPopup name={site.vanityName + " - " + site.annualTotal}/>
+    if (!input) {
+      return 20;
+    }
+
+    let radius=((input / dataMax) * sizeMax) + 20;
+    return radius;
+  }
+
+  render() {
+    const { sites } = this.props;
+
+    if (sites) {
+
+      const markers = sites.map((site, index) => (
         <Circle
-        center={site.geometry}
-        radius={100}
-        />
-      </Marker>
-    ));
-    return <Fragment>{markers}</Fragment>
-  } else {
-    return null;
+          center={site.geometry}
+          radius={this.calculateCircleSize(site.annualTotal)}
+        >
+          <MarkerPopup name={site.vanityName + " - " + site.annualTotal}/>
+        </Circle>
+      ));
+      return <Fragment>{markers}</Fragment>
+    } else {
+      return null;
+    }
   }
 }
-
-export default SiteMarker;
