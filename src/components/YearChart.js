d56f400b18296051755c1e6dec4397380cb76de3
George Ziady <george@springfisher.com>
2026-05-01 11:05:43 +0200

    Update years through 2032.

--- src/components/YearChart.js
+++ src/components/YearChart.js
@@ -19,7 +19,15 @@ export default class YearChart extends Component {
       ["2022", "#eb9595"],
       ["2023", "#9ab7d3"],
       ["2024", "#e3a7c0"],
-    ]
+      ["2025", "#b8f3dd"], // Soft, light blue
+      ["2026", "#90ee90"], // Pastel green
+      ["2027", "#d0b3ff"], // Light purple
+      ["2028", "#ffd5b1"], // Light orange
+      ["2029", "#ffe6f2"], // Soft pink
+      ["2030", "#b7e5d4"], // Light teal
+      ["2031", "#c7e9b4"], // Light mint green
+      ["2032", "#fde0dd"], // Soft peach
+    ];
 
     const years = yearList.map((item, index) => {
       let year = item[0];
@@ -30,6 +38,8 @@ export default class YearChart extends Component {
       // Only show years that are current or in the past, based on user device.
       if (year <= currentYear) {
         return <Bar type="monotone" dataKey={year} fill={colour} />
+      } else {
+        return null;
       }
     });
 
